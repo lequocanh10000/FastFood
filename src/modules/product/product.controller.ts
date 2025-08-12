9c59d91d44865058d1aabfcd261134ba9b1e805a
Tomoriu Akarin <lequocanh10000@github.com>
2025-08-12 15:36:28 +0700

    feat: add product controller

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  // Todo: Thêm check role, admin
+  @Post('create')
+  async create(@Body() createProductDto: CreateProductDto) {
+    return await this.productService.create(createProductDto);
+  }
+
+  @Get('one/:id')
+  async findOne(@Param('id') id: number) {
+    return await this.productService.findOne(id);
+  }
 }
