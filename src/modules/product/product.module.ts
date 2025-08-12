9d9ea2fee245c6091f46c917d7600ee9ca85dde9
Tomoriu Akarin <lequocanh10000@github.com>
2025-08-12 15:36:54 +0700

    feat: import model for product module

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Ingredient, Product, ProductIngredient, ProductVariant } from 'src/models';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
+  imports: [SequelizeModule.forFeature([Product, ProductVariant, Ingredient, ProductIngredient]), CategoryModule],
 })
 export class ProductModule {}
